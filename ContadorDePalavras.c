370b3aff1fbd84f05835c6b1b1ad81a56c181922
handyman0 <118775048+handyman0@users.noreply.github.com>
2023-11-10 20:09:38 -0300

    Update ContadorDePalavras.c
    - Substituí scanf por fgets para a entrada do nome do arquivo, evitando problemas com buffer overflow.
    - Adicionada uma verificação para garantir que a palavra seja contada corretamente, mesmo que ocorra parcialmente em duas linhas diferentes.
    - Adicionado #define MAX_LEN 100 para o comprimento máximo da palavra e do arquivo, tornando mais fácil ajustar esses valores no futuro.
    
    Essas mudanças visam melhorar a segurança e a precisão do programa.

--- ContadorDePalavras.c
+++ ContadorDePalavras.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// Definimos uma constante de 100 para definir o comprimento máximo de uma palavra ou linha no arquivo
+
 #define MAX_LEN 100
 
 int main() {
     FILE *fp;
-    // variáveis com o comprimento maximo definido e a variavel count iniciada com 0
     char arquivo[MAX_LEN], palavra[MAX_LEN], buffer[MAX_LEN];
     int count = 0;
-    // entradas
-    printf("Digite o nome do arquivo de texto: ");
-    scanf("%s", arquivo);
 
-    printf("digite a palavra que deseja contar: ");
+    // Entrada segura para o nome do arquivo
+    printf("Digite o nome do arquivo de texto: ");
+    fgets(arquivo, MAX_LEN, stdin);
+    arquivo[strcspn(arquivo, "\n")] = '\0';  // Remover o caractere de nova linha, se presente
+
+    // Entrada para a palavra a ser contada
+    printf("Digite a palavra que deseja contar: ");
     scanf("%s", palavra);
-    // usei a função fopen para verificar se o arquivo foi aberto corretamente
+
     fp = fopen(arquivo, "r");
     if (fp == NULL) {
         printf("Erro ao abrir o arquivo!\n");
         exit(1);
     }
-    // usei a função fgets para ler cada linha do arquivo em buffer
+
     while (fgets(buffer, MAX_LEN, fp) != NULL) {
         char *ptr = buffer;
+        size_t palavraLen = strlen(palavra);
+
         while ((ptr = strstr(ptr, palavra)) != NULL) {
-            count++;
+            // Verifica se a palavra ocorre completamente ou parcialmente na linha
+            if ((ptr == buffer || *(ptr - 1) == ' ' || *(ptr - 1) == '\t') &&
+                (ptr[palavraLen] == '\0' || ptr[palavraLen] == ' ' || ptr[palavraLen] == '\t' || ptr[palavraLen] == '\n')) {
+                count++;
+            }
             ptr++;
         }
     }
 
     printf("A palavra '%s' ocorre %d vezes no arquivo.\n", palavra, count);
 
-    fclose(fp); //para fechar o arquivo 
+    fclose(fp);
     return 0;
-
-
 }
